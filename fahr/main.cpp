9c140b957e8a43f83ec213de7f2b6533af6498e7
ALUMNO-D <ALUMNO-D@UCSP.AULAB>
2017-08-24 09:57:00 -0500

    ddw

---
+++ fahr/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int fahr;
+
+
+
+
+    for(int fahr =0;fahr<=300;fahr=fahr+20){
+
+
+        cout<<fahr <<"\t"<< (5/9) * (fahr-32) << endl;
+    }
+
+}
